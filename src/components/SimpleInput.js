3cb9ea4d9e7b5c268a3d323ffa57934059962b9c
Serge Byusa <sergebyusa@Serges-MBP.home>
2023-01-01 10:17:57 -0800

    Added the overall formValidation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -7,6 +7,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsValid = !enteredNameIsValid && enteredNameTouched
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid){
+    formIsValid = true
+  }
+   
 
   const nameInputChangeHandler = event  => {
     setEnteredName(event.target.value);
@@ -54,7 +60,7 @@ const SimpleInput = (props) => {
         <p className="error-text">Name must not be empty</p>
       )}
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
